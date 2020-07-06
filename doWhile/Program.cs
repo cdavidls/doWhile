1592ec06131bf1d48b37a23c8d72db1a829d29af
David <dcicero493@gmail.com>
2020-07-06 13:55:25 -0300

    Exemplo doWhile

---
+++ doWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 18;
+            bool adicionar = true;
+
+            do
+            {
+                idade++;
+                Console.WriteLine(idade);
+            } while (adicionar == false);
+
+            Console.ReadKey();
+        }
+    }
+}
